7330fb9626b62f05cf9f6897bdff34e877530d2a
Vasyl Skorbatiuk <v.skorbatyuk@gmail.com>
2022-10-21 10:50:48 +0200

    add localdatetime converter

--- app/src/main/java/com/silverorange/videoplayer/di/AppModule.kt
+++ app/src/main/java/com/silverorange/videoplayer/di/AppModule.kt
@@ -1,6 +1,12 @@
 package com.silverorange.videoplayer.di
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import com.silverorange.videoplayer.AppConsts
+import com.silverorange.videoplayer.data.VideosInfoRepositoryImpl
+import com.silverorange.videoplayer.data.videos.VideosInfoRepository
 import com.silverorange.videoplayer.data.videos.mappers.VideosInfoMapper
 import com.silverorange.videoplayer.data.videos.mappers.VideosInfoMapperImpl
 import com.silverorange.videoplayer.data.videos.remote.VideosApi
@@ -10,7 +16,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
 import javax.inject.Singleton
 
 @Module
@@ -34,9 +47,32 @@ object AppModule {
     @Provides
     fun provideVideosInfoMapper(): VideosInfoMapper = VideosInfoMapperImpl()
 
+    @Singleton
+    @Provides
+    fun provideVideosInfoRepository(
+        dataSource: VideosInfoRemoteDataSource,
+        mapper: VideosInfoMapper
+    ): VideosInfoRepository = VideosInfoRepositoryImpl(dataSource, mapper)
+
     private fun prepareRetrofitConfiguration(): Retrofit {
+        val gsonBuilder = GsonBuilder().registerTypeAdapter(
+            LocalDateTime::class.java,
+            object : JsonDeserializer<LocalDateTime> {
+                override fun deserialize(
+                    json: JsonElement?,
+                    typeOfT: Type?,
+                    context: JsonDeserializationContext?
+                ): LocalDateTime {
+                    return json?.asString?.let {
+                        Instant.parse(it).toLocalDateTime(TimeZone.currentSystemDefault())
+                    } ?: Instant.DISTANT_PAST.toLocalDateTime(TimeZone.currentSystemDefault())
+                }
+            }).create()
+
         return Retrofit.Builder()
             .baseUrl(AppConsts.BASE_API_URL)
+            .client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
             .build()
     }
 }
