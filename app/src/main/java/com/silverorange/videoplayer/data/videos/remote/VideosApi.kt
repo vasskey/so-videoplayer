2da1d55c28a5e1172e736e6830030e585327522d
Vasyl Skorbatiuk <v.skorbatyuk@gmail.com>
2022-10-21 10:47:16 +0200

    switch context to IO when getting videos

--- app/src/main/java/com/silverorange/videoplayer/data/videos/remote/VideosApi.kt
+++ app/src/main/java/com/silverorange/videoplayer/data/videos/remote/VideosApi.kt
@@ -1,10 +1,9 @@
 package com.silverorange.videoplayer.data.videos.remote
 
 import com.silverorange.videoplayer.data.videos.dtos.VideoInfoDto
-import retrofit2.Response
 import retrofit2.http.GET
 
 interface VideosApi {
     @GET("/videos")
-    suspend fun getVideos() : Response<List<VideoInfoDto>>
+    suspend fun getVideos() : List<VideoInfoDto>
 }
--- app/src/main/java/com/silverorange/videoplayer/data/videos/remote/VideosInfoRemoteDataSourceImpl.kt
+++ app/src/main/java/com/silverorange/videoplayer/data/videos/remote/VideosInfoRemoteDataSourceImpl.kt
@@ -1,15 +1,13 @@
 package com.silverorange.videoplayer.data.videos.remote
 
 import com.silverorange.videoplayer.data.videos.dtos.VideoInfoDto
-import com.silverorange.videoplayer.exceptions.ApiResponseException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class VideosInfoRemoteDataSourceImpl @Inject constructor(private val videosApi: VideosApi) : VideosInfoRemoteDataSource {
+class VideosInfoRemoteDataSourceImpl @Inject constructor(private val videosApi: VideosApi) :
+    VideosInfoRemoteDataSource {
     override suspend fun getAvailableVideos(): List<VideoInfoDto> {
-        val videosResponse = videosApi.getVideos()
-        if (videosResponse.isSuccessful)
-            return videosResponse.body()
-        else
-            throw ApiResponseException()
+        return withContext(Dispatchers.IO) { videosApi.getVideos() }
     }
 }
